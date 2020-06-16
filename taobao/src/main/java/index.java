81f39912679dfac3b5c98bfd0d0581a1843d81f7
JUNK-CHUAN <44919630+JUNK-CHUAN@users.noreply.github.com>
2020-06-16 12:07:40 +0800

    initial
    initial

---
+++ taobao/src/main/java/index.java
@@ -0,0 +1,12 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet(value="/index.do")
+public class index extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/index.jsp");
+    }
+}
