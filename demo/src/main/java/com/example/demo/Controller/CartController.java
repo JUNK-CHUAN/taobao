29025cafefab59fd8cc9ecf7c99f76671d9b4d5e
915615783 <915615783@qq.com>
2020-06-18 22:13:05 +0800

    add insertCart api

---
+++ demo/src/main/java/com/example/demo/Controller/CartController.java
@@ -0,0 +1,42 @@
+package com.example.demo.Controller;
+
+import com.example.demo.DAO.CartMapper;
+import com.example.demo.Entity.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class CartController {
+    @Autowired
+    CartMapper CAM;
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/insertCart", method = RequestMethod.POST)
+    @ResponseBody
+    public Map insertCart(@RequestParam(value = "spID") int spID,
+                        @RequestParam(value = "cID") String cID,
+                        @RequestParam(value = "count") int count,
+                        HttpSession session) throws Exception{
+        Map<String, Object> map = new HashMap<>();
+        if(session.getAttribute("isLogIn") == null) {
+            map.put("result", "false");   // 还没登陆
+            System.out.println("还没登录");
+            return map;
+        }
+        List<Cart> cartList = CAM.selectCart(spID, cID);
+        if(cartList.size()!=0){
+            CAM.cartAddCount(spID, cID, count);
+        }
+        else{
+            CAM.insertCart(spID, cID, count);
+        }
+        map.put("result", "true");
+        return map;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/DAO/CartMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.DAO;
+
+import com.example.demo.Entity.Cart;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CartMapper {
+    void insertCart(int spID, String cID, int count);
+    List<Cart> selectCartBycID(String cID);
+    List<Cart> selectCart(int spID, String cID);
+    void deleteCart(int spID, String cID);
+    void cartAddCount(int spID, String cID, int count);
+}
